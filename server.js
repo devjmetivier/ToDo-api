0adf5af2f1a226f336410f98e3af1046881a4dfb
dmetivier <devinmetivier@gmail.com>
2016-08-04 12:14:29 -0400

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/todos/', function (req, res) {
     res.json(body);
 });
 
-//DELETE /todos/:id
+// DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos, {id: todoId});
@@ -57,6 +57,33 @@ app.delete('/todos/:id', function (req, res) {
         todos = _.without(todos, matchedTodo);
         return res.json(matchedTodo);
     }
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
