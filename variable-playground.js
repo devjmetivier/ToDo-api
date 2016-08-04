320241e4cd28dab0603df78d9b4d1f91c4666329
dmetivier <devinmetivier@gmail.com>
2016-08-04 12:53:28 -0400

    Add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+//     name: 'Devin',
+//     age: 20
+// };
+//
+// function updatePerson(obj) {
+//     // obj = {
+//     //     name: 'Devin',
+//     //     age: 21
+//     // }
+//     obj.age = 21;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+var grades = [12,59,46,17];
+
+function addGrade(gradesArr) {
+    gradesArr.push(31);
+    debugger;
+}
+
+addGrade(grades);
+console.log(grades);
