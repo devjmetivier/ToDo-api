58143e2d7322f146e42c03cf552a8c5c70e172d1
dmetivier <devinmetivier@gmail.com>
2016-08-04 12:52:51 -0400

    Add ?completed filter

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ app.get('/', function (req, res) {
 
 // GET /todos
 app.get('/todos', function (req, res) {
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(filteredTodos, {completed: true});
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(filteredTodos, {completed: false});
+    }
+
+    res.json(filteredTodos);
 });
 
 // GET /todos/:id
