b2d616181e7e11d2d992cb4a53e4c9cdbb1756af
dmetivier <devinmetivier@gmail.com>
2016-08-04 13:10:45 -0400

    final static todos

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.get('/todos', function (req, res) {
         filteredTodos = _.where(filteredTodos, {completed: false});
     }
 
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+        filteredTodos = _.filter(filteredTodos, function (todo) {
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+        });
+    }
+
     res.json(filteredTodos);
 });
 
