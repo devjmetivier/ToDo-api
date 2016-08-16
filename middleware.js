6f0595a30ebf69270c6c28e7aeabd93f1282b56d
dmetivier <devinmetivier@gmail.com>
2016-08-15 22:49:30 -0400

    Added DELETE /users/login

--- middleware.js
+++ middleware.js
@@ -1,14 +1,26 @@
-module.exports = function (db) {
+var crypto = require('crypto-js');
 
+module.exports = function (db) {
 
     return {
         requireAuthentication: function (req, res, next) {
-            var token = req.get('Auth');
+            var token = req.get('Auth') || '';
 
-            db.user.findByToken(token).then(function (user) {
+            db.token.findOne({
+                where: {
+                    tokenHash: crypto.MD5(token).toString()
+                }
+            }).then(function (tokenInstance) {
+                if (!tokenInstance) {
+                    throw new Error();
+                }
+
+                req.token = tokenInstance;
+                return db.user.findByToken(token);
+            }).then(function (user) {
                 req.user = user;
                 next();
-            }, function (e) {
+            }).catch(function () {
                 res.status(401).send();
             });
         }
